8b846e7aab5d7d994d931e6877b28877306b9f15
Lucas <lwae2508@gmail.com>
2025-11-04 10:51:41 -0300

    Adicionando comentários

--- jogo.py
+++ jogo.py
@@ -9,11 +9,13 @@ game = True
 
 while game:
     for event in pygame.event.get():
+        #Verifica se apertou o x para sair
         if event.type == pygame.QUIT:
             game = False
-
+        # Verifica se alguma tecla foi clicada
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
+                #Definindo o esc como opção de finalizar o jogo
                 game = False
     window.fill((255,255,255))
 
